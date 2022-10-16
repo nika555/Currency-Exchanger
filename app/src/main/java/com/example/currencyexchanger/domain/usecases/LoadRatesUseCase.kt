0d1f2d2edcf7c8f8b4e92c3fd6fa7dc7e216120b
nika555 <nikatabatadze9@gmail.com>
2022-10-16 21:53:58 +0400

    Remove test invoke fun from usecase

--- app/src/main/java/com/example/currencyexchanger/domain/usecases/LoadRatesUseCase.kt
+++ app/src/main/java/com/example/currencyexchanger/domain/usecases/LoadRatesUseCase.kt
@@ -1,7 +1,5 @@
 package com.example.currencyexchanger.domain.usecases
 
-import com.example.currencyexchanger.common.Resource
-import com.example.currencyexchanger.domain.models.RateModel
 import com.example.currencyexchanger.domain.repositories.RatesRepository
 import javax.inject.Inject
 
@@ -9,8 +7,4 @@ class LoadRatesUseCase @Inject constructor(
     private val repository: RatesRepository
 ) {
     suspend operator fun invoke() = repository.getRates()
-
-    suspend  fun invoke2() : Resource<RateModel> {
-        return Resource.Success(success = null)
-    }
 }
